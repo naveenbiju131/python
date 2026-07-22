087284852e3ac6a96bb8592a5d64890518ac6f46
Jayesh <jayeshmohan30@gmail.com>
2026-07-22 14:21:37 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,73 @@
+# age=18                        if statemnet
+# if age>=18:
+#     print("correct")
+
+# print("hello")
+
+
+
+# age =17                  if else statement 
+# if age>=18:
+#     print("correct")
+# else:
+#     print("false")
+
+
+
+# day=3                    elif
+# if day==1:
+#     print("sunday")
+# elif day==2:
+#     print("monday")
+# elif day==3:
+#     print ("tuesday")
+# elif day==4:
+#     print ("wednesday")
+# elif day==5:
+#     print("thursday")
+# elif day==6:
+#     print ("friday")
+# elif day==7:
+#     print("saturday")
+# else:
+#     print("invalid day")
+
+
+  
+# age=18
+# student=True
+
+# if age>=18:
+#     if student:
+#         print("You can vote")
+#     else:
+#         print ("You can't vote")
+# else:
+#     print("you can't vote")
+
+
+# age=50
+# if age>=50:              .1
+#      print("correct")
+
+
+# if age % 5== 0:          .2
+#     print("age is divisible by 5")
+
+
+# if age >=18:             .3
+#     print("age is above 18")
+
+
+
+
+# a=int(input("enter a number : "))     .4
+# if a <0:
+#     print("number is negative")
+    
+
+
+a=int(input("enter marks: "))
+
+
+
---
+++ naveen.py
@@ -0,0 +1,102 @@
+# print("hlo")
+# A=10
+# A=20
+# print (A)
+# hello=10
+# _hello=20
+# Hello=30
+# HELLO=40
+# heLlo=50
+# print (hello)
+# print (_hello)
+# print (Hello)
+# print (HELLO)
+# print (heLlo)
+
+
+
+# a=b=c=10
+# print (a)
+# print (b)
+# print (c)
+# a,b,c=10,20,30
+# print (a)
+# print (b)
+# print (c)
+# a=10
+# b=20
+# # a,b=b,a
+# print (a)
+# print (b)
+
+
+
+# a=5
+# b=3
+# print (a+b)
+# # print (a-b)
+# # print (a*b)
+# # print (a/b)
+# # print (a%b)
+# # print (a**b)
+# # print (a//b)
+
+
+
+# a=5
+# a=a+5
+# print (a)
+# a-=5
+# print (a)
+# a*=5
+# print (a)
+# a/=5
+# print (a)
+# a%=5
+# print (a)
+# a**=5
+# print (a)
+# a//=5
+# print (a)
+
+
+
+# age=18
+# print (age==18)
+# print (age>18)
+# print (age<18)
+# print (age >=18)
+# print (age<=18)
+# print (age!=18)
+
+
+
+# age=20
+# student =False
+# print (age>=20 and student)
+# print (age >=20 or student)
+# print (not student )
+
+
+
+# x=5
+# y=3
+# print (x&y)
+# print (x|y)
+# print (x^y)
+# mylist=[1,2,3,4,5]
+# print (2 not in mylist)
+
+
+
+# a=[1,2,3]
+# b=a
+# c=[1,2,3]
+# print (a is b)
+# print (a is c)
+# a=10
+# print (type(a))
+# name= {1,2,3,4,5}
+# print (type(name))
+person={"name":"nav","age":22}
+print (type(person))
